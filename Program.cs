75cf059aa9e6a9f22b14d79ff468fe503cf818d5
Carlos Eduardo <40584880+carlosedu13@users.noreply.github.com>
2019-08-05 00:32:36 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace WebScrappingGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "https://github.com/carlosedu13/LandingCurriculum";
+            WebClient LocalWebClient = new WebClient();
+            string Page = LocalWebClient.DownloadString(url);
+
+            HtmlDocument HTMLDocument = new HtmlDocument();
+            HTMLDocument.LoadHtml(Page);
+
+            const string Xpath = "//div[contains(@class, 'content')"; // Trying select the class
+            var Nodes = HTMLDocument.DocumentNode.SelectNodes(Xpath);
+            Console.WriteLine(Nodes);
+            //.Descendants().First(x => x.attributes["class"] != null && x.Attributes["class"].Value.Equals("overall")).InnerText;
+            //WebUtility.HtmlDecode();
+            
+        }
+    }
+}
